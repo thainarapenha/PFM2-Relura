2eb62b93ba65ba28c7e1acdbbb3ac7eb5a07faa1
Thainara Penha <thainarapenha@alu.ufc.br>
2023-08-24 07:40:55 -0300

    refactor: altera 'página' de login

--- src/JavaScript/validaçãoCEP.js
+++ src/JavaScript/validaçãoCEP.js
@@ -1,40 +1,37 @@
-//Jquery Mask
 $('#inputcep').mask('00000-000');
 
-//função para limparCampos
 function limparCampos() {
-    $("#inputcep").val("")
-    $("#Estado").val("")
-    $("#Cidade").val("")
-    $("#Bairro").val("")
-    $("#Rua").val("")   
+  $("#inputcep").val("")
+  $("#Estado").val("")
+  $("#Cidade").val("")
+  $("#Bairro").val("")
+  $("#Rua").val("")
 }
 
-//validação CEP
 $("#inputcep").blur(function () {
-    let CEP = $("#inputcep").val().replace("-", "")
-    if (CEP.length === 8) {
-        $.get(`https://viacep.com.br/ws/${CEP}/json`, function (JSON) {
-            try {
-                if (!("erro" in JSON)) {
-                    $("#Estado").val(JSON.uf)
-                    $("#Cidade").val(JSON.localidade)
-                    $("#Bairro").val(JSON.bairro)
-                    $("#Rua").val(JSON.logradouro)
-                } else {
-                    throw new Error("CEP invalido")
-                }
-            } catch (e) {
-                console.log(e.message)
-                alert("Digite um CEP valido")
-                limparCampos()
-            }
-        })
-    } else if (CEP.length < 8 && CEP !== "") {
+  let CEP = $("#inputcep").val().replace("-", "")
+  if (CEP.length === 8) {
+    $.get(`https://viacep.com.br/ws/${CEP}/json`, function (JSON) {
+      try {
+        if (!("erro" in JSON)) {
+          $("#Estado").val(JSON.uf)
+          $("#Cidade").val(JSON.localidade)
+          $("#Bairro").val(JSON.bairro)
+          $("#Rua").val(JSON.logradouro)
+        } else {
+          throw new Error("CEP invalido")
+        }
+      } catch (e) {
+        console.log(e.message)
         alert("Digite um CEP valido")
         limparCampos()
-    } else {
-        alert("Digite um CEP")
-        limparCampos()
-    }
+      }
+    })
+  } else if (CEP.length < 8 && CEP !== "") {
+    alert("Digite um CEP valido")
+    limparCampos()
+  } else {
+    alert("Digite um CEP")
+    limparCampos()
+  }
 })
